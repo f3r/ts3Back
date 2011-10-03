a76a907f716142a20d6902fd6132235fd8f5835a
Fer Martin <fernandomartinsantana@gmail.com>
2011-10-03 12:13:07 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeyPalBackEnd::Application.config.secret_token = '4fafcf33a55a5b7d6cd2beebb26368869e9f450b65ea004e4243e7a6b3201d5f1e2da0ce7310102c7d481c05b459b0d100087f73545fd14aff0de8050f818a61'
