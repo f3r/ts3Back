4f6db0a2112140ed40b51a4c762de02b8ff4ea55
Jorge Yau <jorgeyau@gmail.com>
2011-10-05 12:30:24 -0500

    Added registrations/destroy

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel, :destroy ]
+  prepend_before_filter :authenticate_scope!, :only => [:edit, :update]
   skip_before_filter :verify_authenticity_token
 
   # ==Resource URL
@@ -14,13 +16,39 @@ class RegistrationsController < Devise::RegistrationsController
     resource = resource_class.new_with_session(parameters, session)
     respond_to do |format|
       if resource.save
-          response = { :stat => "ok", :msg => I18n.t("devise.registrations.signed_up") }
+          response = { :stat => "ok", :user_id => resource.id, :msg => I18n.t("devise.registrations.signed_up") }
           format.json { render :status => 200, :json => response }
           format.xml { render :status => 200, :xml => response.to_xml(:root => "rsp") }
       else
           response = { :stat => "fail", :err => resource.errors }
-          format.json { render :status => 401, :json => response.to_json }
-          format.xml { render :status => 401, :xml => response.to_xml(:root => "rsp") }
+          format.json { render :status => 200, :json => response.to_json }
+          format.xml { render :status => 200, :xml => response.to_xml(:root => "rsp") }
+      end
+    end
+  end
+
+  # ==Resource URL
+  # http://backend-heypal.heroku.com/users.format
+  # ==Example
+  # DELETE http://backend-heypal.heroku.com/users.json id=id
+  # === Parameters
+  # [:id]
+  def destroy
+    parameters = {:id => params[:id]}
+    begin
+      resource = User.find(params[:id])
+    rescue Exception => e
+      error_message = e.message
+    end
+    respond_to do |format|
+      if resource && resource.destroy
+        response = { :stat => "ok", :msg => I18n.t("devise.registrations.destroyed") }
+        format.json { render :status => 200, :json => response }
+        format.xml { render :status => 200, :xml => response.to_xml(:root => "rsp") }
+      else
+        response = { :stat => "fail", :err => error_message }
+        format.json { render :status => 200, :json => response }
+        format.xml { render :status => 200, :xml => response.to_xml(:root => "rsp") }
       end
     end
   end
