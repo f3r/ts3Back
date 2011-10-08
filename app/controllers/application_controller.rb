d658626e2f4f3c727c69e0b97cff0288cb7acb0b
Jorge Yau <jorgeyau@gmail.com>
2011-10-07 21:47:12 -0500

    Added exception catchers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  include Exceptions
+  rescue_from Exceptions::UnauthorizedAccess, :with => :unauthorized_access
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  rescue_from ActiveRecord::RecordInvalid, :with => :not_found
+  rescue_from ActionController::UnknownAction, :with => :not_found
+
   protect_from_forgery
 
   def format_response(response,format)
@@ -21,14 +25,20 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    warden.user || User.find_for_token_authentication(:auth_token => params[:access_token])
+  end
+
   def unauthorized_access
     respond_with do |format|
       format.any(:xml, :json) { render :status => 401, request.format.to_sym => format_response({ :stat => "fail", :msg => I18n.t("devise.failure.invalid_token") },request.format.to_sym) }
     end
   end
 
-  def current_user
-    warden.user || User.find_for_token_authentication(:auth_token => params[:access_token])
+  def not_found
+    respond_with do |format|
+      format.any(:xml, :json) { render :status => 404, request.format.to_sym => format_response({ :stat => "fail", :err => I18n.t("not_found") },request.format.to_sym) }
+    end
   end
 
 end
