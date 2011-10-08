2fb0573b72ebd8e214ee7cbd8ac3caccf4073150
Jorge Yau <jorgeyau@gmail.com>
2011-10-08 03:27:05 -0500

    testing

--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -1,29 +1,37 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
+  setup do
+    @category = categories(:one)
+  end
+
   test "should get index" do
-    get :index
+    get :index, :format => :json, :access_token => "testing"
     assert_response :success
+    assert_not_nil assigns(:categories)
   end
 
-  test "should get create" do
-    get :create
+  test "should create category" do
+    assert_difference('Category.count') do
+      post :create, category: @category.attributes, :format => :json, :access_token => "testing"
+    end
     assert_response :success
   end
-
-  test "should get show" do
-    get :show
+  
+  test "should show category" do
+    get :show, id: @category.to_param, :format => :json, :access_token => "testing"
     assert_response :success
   end
-
-  test "should get update" do
-    get :update
+  
+  test "should update category" do
+    put :update, id: @category.to_param, category: @category.attributes, :format => :json, :access_token => "testing"
     assert_response :success
   end
-
-  test "should get destroy" do
-    get :destroy
+  
+  test "should destroy category" do
+    assert_difference('Category.count', -1) do
+      delete :destroy, id: @category.to_param, :format => :json, :access_token => "testing"
+    end
     assert_response :success
   end
-
 end
