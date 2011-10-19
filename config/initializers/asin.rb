3cce54b51f02d94529d1aa4714ee7b7c6484c444
Fer Martin <fer@heypal.com>
2011-10-19 16:03:46 +0800

    Added associate-id for amazon querying

--- config/initializers/asin.rb
+++ config/initializers/asin.rb
@@ -1,5 +1,5 @@
 ASIN::Configuration.configure do |config|
   config.secret = 'KZukEWwb6soc8BwY7yi31Xon9CUajK0vjxiKEdQK'
   config.key    = 'AKIAIB4COIY2DHN3FLTA'
-  #config.associate_tag = 'your-tag'
+  config.associate_tag = 'heypal-20'
 end
