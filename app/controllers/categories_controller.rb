0b9ae8faba944e73ffebbfd3db776bc31b7643c6
Jorge Yau <jorgeyau@gmail.com>
2011-10-21 11:46:00 -0500

    Added categories tests, refactored methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,8 @@ class CategoriesController < ApplicationController
   skip_before_filter :verify_authenticity_token
   respond_to :xml, :json
   # TODO: protect everything but /list with admin token
-  # TODO: Create tests
 
+  # TODO: Do we need this method?
   # ==Resource URL
   # /categories.format
   # ==Example
@@ -43,8 +43,12 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     respond_with do |format|
-      response = @category ? { :stat => "ok", :category => @category } : { :stat => "fail", :err => @category.errors }
-      format.any(:xml, :json) { render :status => 200, request.format.to_sym => format_response(response,request.format.to_sym) }
+      format.any(:xml, :json) { 
+        render :status => 200, 
+        request.format.to_sym => format_response({ 
+          :stat => "ok", 
+          :category => @category },
+          request.format.to_sym) }
     end
   end
 
@@ -93,9 +97,19 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     respond_with do |format|
       if @category.update_attributes(:name => params[:name])
-        format.any(:xml, :json) { render :status => 200, request.format.to_sym => format_response({ :stat => "ok", :category => @category, :msg => I18n.t("successfully_updated", :object_name => t(@category.class.to_s.downcase), :name => @category.name) },request.format.to_sym) }
+        format.any(:xml, :json) { 
+          render :status => 200, 
+          request.format.to_sym => format_response({ 
+            :stat => "ok", 
+            :category => @category },
+          request.format.to_sym) }
       else
-        format.any(:xml, :json) { render :status => 200, request.format.to_sym => format_response({ :stat => "fail", :err => @category.errors },request.format.to_sym) }
+        format.any(:xml, :json) { 
+          render :status => 200, 
+          request.format.to_sym => format_response({ 
+            :stat => "fail", 
+            :err => format_errors(@category.errors.messages) },
+          request.format.to_sym) }
       end
     end
   end
@@ -111,9 +125,19 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     respond_with do |format|
       if @category.destroy
-        format.any(:xml, :json) { render :status => 200, request.format.to_sym => format_response({ :stat => "ok", :category => @category, :msg => I18n.t("successfully_deleted", :object_name => t(@category.class.to_s.downcase), :name => @category.name) },request.format.to_sym) }
+        format.any(:xml, :json) { 
+          render :status => 200, 
+          request.format.to_sym => format_response({ 
+            :stat => "ok", 
+            :category => @category },
+          request.format.to_sym) }
       else
-        format.any(:xml, :json) { render :status => 200, request.format.to_sym => format_response({ :stat => "fail", :err => @category.errors },request.format.to_sym) }
+        format.any(:xml, :json) { 
+          render :status => 200, 
+          request.format.to_sym => format_response({ 
+            :stat => "fail", 
+            :err => format_errors(@category.errors.messages) },
+          request.format.to_sym) }
       end
     end
   end
