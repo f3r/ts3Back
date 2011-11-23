3603c2ea292791f228463aee8d4b693b1f69da21
Jorge Yau <jorgeyau@gmail.com>
2011-11-23 15:33:31 -0500

    age calculation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,7 @@ class User < ActiveRecord::Base
 
   def age
     now = Time.now.utc.to_date
-    now.year - birthdate.year - (birthdate.to_date.change(:year => now.year) > now ? 1 : 0)
+    now.year - birthdate.year - ((now.month > birthdate.month || (now.month == birthdate.month && now.day >= birthdate.day)) ? 0 : 1)
   end
   
   def activated?
