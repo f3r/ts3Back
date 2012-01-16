f9211c180f8d181284610b6266117a4dd7b81f31
Fer Martin <fer@heypal.com>
2012-01-16 13:21:09 +0800

    [FIXES #23384423] Messaging privacy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -84,7 +84,7 @@ class MessagesController < ApplicationController
     if Message.to(params['id'], params['message'])
       user = User.find(params['id'])
       UserMailer.new_message(user).deliver
-      UserMailer.new_message_admin(current_user,user, params['message']).deliver
+      UserMailer.new_message_admin(current_user, user, params['message']).deliver
       return_message(200, :ok)
     else
       return_message(200, :ok, {:err => {:messages => [106]}})
