357810bf86b57f76db1260b5331e4fb4429a130b
Fer Martin <fer@heypal.com>
2012-01-16 13:05:59 +0800

    [FIXES #23384423]

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -39,4 +39,26 @@ class UserMailer < ActionMailer::Base
       logger.error { "Error [user_mailer.rb/new_message] #{e.message}" }
     end
   end  
+
+  # ==Description
+  # Email sent when the user receives a message
+  def new_message_admin(from_user, to_user, message)
+    begin
+      @from_user = from_user
+      @to_user   = to_user
+      @message   = message
+      recipients = "wenwen@heypal.com, jeremy@heypal.com, fer@heypal.com"
+      subject    = "#{@to_user.full_name} has a new message!"
+      sent_on    =  Time.now
+      mail(:from    => MAILER_SENDER, 
+           :to      => recipients, 
+           :subject => subject, 
+           :date    => sent_on) do |format|
+        format.text
+        format.html
+      end
+    rescue Exception => e
+      logger.error { "Error [user_mailer.rb/new_message] #{e.message}" }
+    end
+  end
 end
