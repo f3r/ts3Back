3e0903fcd06c16450fe60ff0ab216374e4d78ea3
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-02-10 13:13:14 +0800

    Adding details to the places admin interface

--- app/admin/places.rb
+++ app/admin/places.rb
@@ -12,19 +12,21 @@ ActiveAdmin.register Place do
   filter :city
   
   index do |place|
-    column(:id)            {|place| link_to "##{place.id} ", admin_place_path(place) }
+    id_column
     column :title
     column :user
     column :city
     column :created_at
     column :updated_at
-    column("Status")       {|place| status_tag(place.published ? 'Published' : 'Unpublished') }
+    column("Status")      {|place| status_tag(place.published ? 'Published' : 'Unpublished') }
+    column("Actions")     {|place| place_links_column(place) }
   end
   
   show do |ad|
     rows = default_attribute_table_rows.reject {|a| a =~ /photos|amenities|review/}
     attributes_table *rows do
-      row(:amenties)      {|place| amenities_row(place)}
+      row(:amenties)      {|place| place_amenities_row(place) }
+      row(:photos)        {|place| place_photos_row(place)}
     end
   end
 end
--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,5 +1,15 @@
 module PlacesHelper
-  def amenities_row(place)
+  def public_place_path(place)
+    frontend_url("/places/#{place.id}")
+  end
+  
+  def place_links_column(place)
+    html = link_to('Details', admin_place_path(place), :class => 'member_link')
+    html << link_to('Edit', edit_admin_place_path(place), :class => 'member_link')
+    html << link_to_if(place.published, 'View Public', public_place_path(place), :class => 'member_link', :target => '_blank')
+  end
+  
+  def place_amenities_row(place)
     amenities = []
     Place.columns.select{|col| col.name =~ /amenities/}.each do |amenity_col|
       if place[amenity_col.name]
@@ -8,5 +18,12 @@ module PlacesHelper
     end
     
     amenities.join(', ')
+  end
+  
+  def place_photos_row(place)
+    return unless place.photos
+    place.photos.collect do |photo|
+      link_to image_tag(photo["photo"]["small"]), photo["photo"]["original"], :target => '_blank'
+    end.join(' ').html_safe
   end
 end
